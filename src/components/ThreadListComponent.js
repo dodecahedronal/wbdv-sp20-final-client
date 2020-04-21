5941c0a80d4fa10146b5d372547e80c70d28857e
unknown <he.sha@husky.neu.edu>
2020-04-21 03:34:26 -0400

    tried to style some threads

--- src/components/ThreadListComponent.js
+++ src/components/ThreadListComponent.js
@@ -3,6 +3,7 @@ import threadService from "../services/ThreadService";
 import {createThread, findThreadsForBook, deleteThread} from "../actions/threadActions";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
+import './Thread.css';
 
 class ThreadListComponent extends React.Component {
     state = {
@@ -18,7 +19,7 @@ class ThreadListComponent extends React.Component {
         return( 
             <div className="thread-list">
                 <h3>Threads</h3>
-                <li>
+                <div className="row add-new-thread">
                     <textarea
                     onChange={(e) => this.setState({
                             currentTitle: e.target.value
@@ -26,18 +27,21 @@ class ThreadListComponent extends React.Component {
                         value={this.state.currentTitle}/>
                     <button onClick={() =>{
                         const thread = {subject: this.state.currentTitle, bookId: this.props.bookId, username: this.props.cookies.get('username'), userId: this.props.cookies.get('uid')}
-                        this.props.createThread(thread)
+                        this.props.createThread(thread);
                         this.setState({currentTitle: "New Thread"})
                     }}>Post!</button>
-                </li>
+                </div>
                     {this.props.threads.map(thread =>
-                    <li className="thread" key={thread._id}>
-                        <Link to={`/book/${this.props.bookId}/thread/${thread._id}`}>
+                    <div className="thread" key={thread._id}>
+                        <Link to={`/book/${this.props.bookId}/thread/${thread._id}`} className="thread-subject">
                         {thread.subject}
                         </Link>
-                        {thread.username}
+                        {thread.userId === this.props.cookies.get('uid') ?
+                            <Link to="/profile" className="thread-author">- {thread.username}</Link> :
+                            <span className="thread-author">- {thread.username}</span>
+                        }
                         {this.props.cookies.get('uid') === thread.userId && <button onClick={()=>this.props.deleteThread(thread._id)}>Delete</button>}
-                    </li>)}
+                    </div>)}
                 </div>
         )
     }
